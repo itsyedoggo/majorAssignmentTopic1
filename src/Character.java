8225ddd11dd3669978fab820c460c90003d2edc3
itsyedoggo <88696353+itsyedoggo@users.noreply.github.com>
2025-04-28 05:13:30 +0000

    added character class

---
+++ src/Character.java
@@ -0,0 +1,30 @@
+public class Character {
+    private String name;
+    private int health;
+    private int attackPower;
+
+    public Character(String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void takeDamage(int damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+
+    public void attack(Character target) {
+        target.takeDamage(this.attackPower);
+    }
+}
