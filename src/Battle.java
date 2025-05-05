cdf9ed8cfd374794bc1eee2a1d8e24f0f4b4e04d
NatalyShenouda <nataly.shenouda@students.mq.edu.au>
2025-05-05 01:05:48 +0000

    1

---
+++ src/Battle.java
@@ -0,0 +1,17 @@
+public class Battle {
+    private Character attacker1;
+    private Character attacker2;
+
+    public Battle(Character attacker1, Character attacker2) {
+        this.attacker1 = attacker1;
+        this.attacker2 = attacker2;
+    }
+
+    public String startFight() {
+        if (!attacker1.isAlive() || !attacker2.isAlive()){
+            return "One of the figheters is already dead!!";
+        }
+        int damage = attacker1.attach(attacker2);
+        return attacker1.getName() + " attacked " + attacker2.getName() + " for " + damage + "damage!";
+    }
+}
--- src/Character.java
+++ src/Character.java
@@ -37,4 +37,5 @@ public class Character {
     public int getHealth() { return health; }
     public String getRace() { return race; }
     public String getCHaracterClass () { return characterClass; }
+}
     //changed
